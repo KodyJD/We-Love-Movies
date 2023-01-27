8447ffbf7ca38e48d7d3c2d27fe476c7fcd50849
KodyJD <113568706+KodyJD@users.noreply.github.com>
2023-01-26 18:29:46 -0800

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://bidhpatw:bcOAq1rykwWZK70LF4AZtKdlqApT-OLV@kashin.db.elephantsql.com/bidhpatw",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
